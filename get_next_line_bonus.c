97698d7d543aaa3ce3bc5abe117757523e284961
juggorr <juggorrvol.2@gmail.com>
2024-01-19 17:50:10 +0900

    [WIP] start!

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,15 +6,26 @@
 /*   By: juggorr <juggorr@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:50:46 by juggorr           #+#    #+#             */
-/*   Updated: 2024/01/19 16:18:34 by juggorr          ###   ########.fr       */
+/*   Updated: 2024/01/19 17:44:31 by juggorr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line_bonus.h"
 #include<stdio.h>
+#include<fcntl.h>
 
+void	read_fd(int fd)
+{
+	char	buf[BUFFER_SIZE];
+	ssize_t	res;
 
+	res = read(fd, buf, BUFFER_SIZE);
+	printf("%s\n", buf);
+}
 
 int	main(void)
 {
+	int	fd0 = open("./text.txt", O_RDONLY);
+	int	fd2 = open("./text2.txt", O_RDONLY);
 
+	read_fd(fd0);
 }
