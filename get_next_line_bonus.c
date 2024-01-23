0666ed0de5b57a71b65b009bbfeff633671bd630
juggorr <juggorrvol.2@gmail.com>
2024-01-23 16:57:35 +0900

    [WIP] stil..

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,33 +6,37 @@
 /*   By: juggorr <juggorr@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:50:46 by juggorr           #+#    #+#             */
-/*   Updated: 2024/01/23 13:19:22 by juggorr          ###   ########.fr       */
+/*   Updated: 2024/01/23 16:57:22 by juggorr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line_bonus.h"
 #include<stdio.h>
-#include<fcntl.h>
+#include<fcntl.h>	
 
-void	read_fd(int fd)
+char	*get_next_line_bonus(int fd)
 {
-	char	*buf = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
+	static	char	*buf[4096];
+	char	*tmp_buf = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
 	ssize_t	res;
 
-	res = read(fd, buf, BUFFER_SIZE);
-	buf[res] = '\0';
+	res = read(fd, tmp_buf, BUFFER_SIZE);
+
+	tmp_buf[res] = '\0';
 	printf("res: %zd\n", res);
-	printf("buf: %s\n", buf);
+	printf("buf: %s\n", tmp_buf);
+	return (tmp_buf);
 }
 
 int	main(void)
 {
 	int	fd1;
 	int	fd2;
+	char	*dst;
 
 	fd1 = open("./text.txt", O_RDONLY);
 	fd2 = open("./text2.txt", O_RDONLY);
-	read_fd(fd1);
-	read_fd(fd2);
-	read_fd(fd1);
-	read_fd(fd2);
+	dst = get_next_line_bonus(fd1);
+	dst = get_next_line_bonus(fd1);
+	dst = get_next_line_bonus(fd2);
+	dst = get_next_line_bonus(fd2);
 }
