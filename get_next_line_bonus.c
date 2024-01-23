4a33ebfb00f1f35fac00b7f08c6310c164951411
juggorr <juggorrvol.2@gmail.com>
2024-01-23 13:21:59 +0900

    [WIP] structure

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: juggorr <juggorr@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:50:46 by juggorr           #+#    #+#             */
-/*   Updated: 2024/01/19 17:44:31 by juggorr          ###   ########.fr       */
+/*   Updated: 2024/01/23 13:19:22 by juggorr          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include"get_next_line_bonus.h"
@@ -15,17 +15,24 @@
 
 void	read_fd(int fd)
 {
-	char	buf[BUFFER_SIZE];
+	char	*buf = (char *)calloc(BUFFER_SIZE + 1, sizeof(char));
 	ssize_t	res;
 
 	res = read(fd, buf, BUFFER_SIZE);
-	printf("%s\n", buf);
+	buf[res] = '\0';
+	printf("res: %zd\n", res);
+	printf("buf: %s\n", buf);
 }
 
 int	main(void)
 {
-	int	fd0 = open("./text.txt", O_RDONLY);
-	int	fd2 = open("./text2.txt", O_RDONLY);
+	int	fd1;
+	int	fd2;
 
-	read_fd(fd0);
+	fd1 = open("./text.txt", O_RDONLY);
+	fd2 = open("./text2.txt", O_RDONLY);
+	read_fd(fd1);
+	read_fd(fd2);
+	read_fd(fd1);
+	read_fd(fd2);
 }
